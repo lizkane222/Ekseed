0edf04349284ac4245df534c3d8c16e76ac6d551
Liz Kane <lizkane222@gmail.com>
2020-10-27 15:08:22 -0700

    git init with file structure-base-1

---
+++ leverage/components/SideBar/Login.js
@@ -0,0 +1,7 @@
+import React, {userEffect} from "react";
+import { NavLink } from "react-router-dom";
+
+import UserModel from "../../models/UserModel";
+
+import { userRecoilState } from "recoil";
+import { userState } from "../../"
---
+++ leverage/config/Routes.js
@@ -0,0 +1,13 @@
+import React from "react";
+import {Switch, Route} from "react-router-dom";
+
+
+
+
+
+
+
+// const Routes = (props) => {
+//     const loggedIn = useRecoilValue(loggedInState);
+
+// }
